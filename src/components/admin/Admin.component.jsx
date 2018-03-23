b078141521730a9001bb9cfd7642d9e6da316cc5
candy_man <Upendo@Pauls-MacBook-Pro-2.local>
2018-03-23 17:25:59 +0300

    ch(KH-web-app):Admin page

--- src/components/admin/Admin.component.jsx
+++ src/components/admin/Admin.component.jsx
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
+import { Breadcrumb } from 'semantic-ui-react';
 
 //styles
 import './Admin.css';
 
+// common components
+import BreadcrumbComponent from '../../common/BreadCrumb.component';
+
 export default class Admin extends Component {
   render() {
-    return <div className="admin-container">Admin Components go here</div>;
+    return (
+      <div className="admin-container">
+        <BreadcrumbComponent page="Admin" selection="Company Info" />
+      </div>
+    );
   }
 }
