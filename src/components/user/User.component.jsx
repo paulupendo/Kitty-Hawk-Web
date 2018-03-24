fd6ca641f635522da8cb1cf5f243609a6320b17b
jimmykimani <lovepaul46@outlook.com>
2018-03-24 19:58:05 +0300

    feat(User-api): add user table

--- src/components/user/User.component.jsx
+++ src/components/user/User.component.jsx
@@ -1,26 +1,24 @@
-import React, { Component } from 'react';
-import { Dropdown, Button, Segment } from 'semantic-ui-react';
+import React, { Component } from "react";
+import { Dropdown, Button, Segment } from "semantic-ui-react";
 
 // styles
-import './User.css';
+import "./User.css";
 
 // components
-import BreadcrumbComponent from '../../common/BreadCrumb.component';
-import { CallOptions } from '../../common/DropdownOptions.component';
+import BreadcrumbComponent from "../../common/BreadCrumb.component";
+import { CallOptions } from "../../common/DropdownOptions.component";
 import GetUsers from "./subComponents/GetUsers/GetUsers.component";
-import GetUser from './subComponents/GetUser/GetUser.component'
-import SubHeader from './SubHeader.component';
-import CreateUser from './subComponents/CreateUser/CreateUser.component';
+import GetUser from "./subComponents/GetUser/GetUser.component";
+import SubHeader from "./SubHeader.component";
+import CreateUser from "./subComponents/CreateUser/CreateUser.component";
 
 export default class User extends Component {
-  state = {
-    activeComponent: 'Get Users',
-  };
+  state = { activeComponent: "Create User" };
 
   data = [
-    { key: 'POST', value: 'Get User', text: 'Get User' },
-    { key: 'GET', value: 'Get Users', text: 'Get Users' },
-    { key: 'POST-user', value: 'Create User', text: 'Create User' },
+    { key: "POST", value: "Get User", text: "Get User" },
+    { key: "GET", value: "Get Users", text: "Get Users" },
+    { key: "POST-user", value: "Create User", text: "Create User" }
   ];
 
   handleChange = (e, { value }) => {
@@ -29,18 +27,24 @@ export default class User extends Component {
 
   switchComponents = () => {
     switch (this.state.activeComponent) {
-      case 'Get User':
-        return <div>
-            <SubHeader info="Create a new User. This requires a unique email address for the
-              User being created" />
+      case "Get User":
+        return (
+          <div>
+            <SubHeader
+              info="Create a new User. This requires a unique email address for the
+              User being created"
+            />
             <GetUser />
-          </div>;
-      case 'Get Users':
-        return <div>
+          </div>
+        );
+      case "Get Users":
+        return (
+          <div>
             <SubHeader info="Allows a caller to request a page with a list of Users resources belonging to a Tenant" />
             <GetUsers />
-          </div>;
-      case 'Create User':
+          </div>
+        );
+      case "Create User":
         return (
           <div>
             <SubHeader
