95fc73024ed0fb5b729e4aa27428ff726db8e93d
candy_man <Upendo@Pauls-MacBook-Pro-2.local>
2018-04-05 15:53:21 +0300

    feat(web-app):add create user functionality

--- src/components/user/subComponents/CreateUser/CreateUser.component.jsx
+++ src/components/user/subComponents/CreateUser/CreateUser.component.jsx
@@ -4,45 +4,64 @@ import { Input, Dropdown } from 'semantic-ui-react';
 // styles
 import './CreateUser.css';
 
-const CreateUser = () => {
+const CreateUser = ({ handleChange, handleDropDownChange }) => {
   const zoneOptions = [
     { key: 'User', value: 'User', text: 'User' },
     { key: 'Administrator', value: 'Administrator', text: 'Administrator' },
-    { key: 'Zone Manager', value: 'Zone Manager', text: 'Zone Manager' }
+    { key: 'Zone Manager', value: 'Zone Manager', text: 'Zone Manager' },
   ];
 
-  return <div className="create-user-container">
+  return (
+    <div className="create-user-container">
       <div className="create-input">
         <div className="btns-row">
           <div>
             <span>First Name</span>
             <br />
-            <Input placeholder="(required):{string}" />
+            <Input
+              placeholder="(required):{string}"
+              onChange={e => handleChange(e, 'first_name')}
+            />
           </div>
           <div>
             <span>Last Name</span>
             <br />
-            <Input placeholder="(required):{string}" />
+            <Input
+              placeholder="(required):{string}"
+              onChange={e => handleChange(e, 'last_name')}
+            />
           </div>
         </div>
         <div className="btns-row">
           <div>
             <span>Email</span>
             <br />
-            <Input placeholder="(required):{string}" />
+            <Input
+              placeholder="(required):{string}"
+              onChange={e => handleChange(e, 'email')}
+            />
+          </div>
+          <div>
+            <span>Zone Id</span> <br />
+            <Input
+              placeholder="(required):{string}"
+              onChange={e => handleChange(e, 'zoneId')}
+            />
           </div>
           <div>
             <span>User Role</span>
             <br />
-            <Dropdown selection placeholder="Select Role" options={zoneOptions} />
-          </div>
-          <div>
-            <span>Zone</span> <br />
-            <Dropdown selection placeholder="Select Zone" options={zoneOptions} />
+            <Dropdown
+              selection
+              placeholder="Select Role"
+              options={zoneOptions}
+              onChange={handleDropDownChange}
+            />
           </div>
         </div>
       </div>
-    </div>;
+    </div>
+  );
 };
 
 export default CreateUser;
